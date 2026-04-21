b95fb07b02ee9911416d98f85205535f919988bb
sasolaso <99582930+sasolaso@users.noreply.github.com>
2026-04-21 13:39:29 +0100

    Await createClient in GET and DELETE functions

--- src/app/api/accounts/route.ts
+++ src/app/api/accounts/route.ts
@@ -1,8 +1,10 @@
+// src/app/api/accounts/route.ts
+
 import { createClient } from '@/lib/supabase/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const {
     data: { user },
@@ -20,7 +22,7 @@ export async function GET(request: NextRequest) {
 }
 
 export async function DELETE(request: NextRequest) {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const {
     data: { user },
